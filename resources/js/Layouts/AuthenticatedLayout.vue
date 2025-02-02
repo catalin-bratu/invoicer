5e1fd10a556d5dd87069c6c8f86d3d79fe42a892
Catalin Bratu <contact@catalinbratu.com>
2025-02-02 11:52:50 +0200

    feat: add theme button

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -11,13 +11,22 @@ import {
 } from '@/Components/ui/dropdown-menu';
 import { Sheet, SheetContent, SheetTrigger } from '@/Components/ui/sheet';
 import { Link } from '@inertiajs/vue3';
-import { HamburgerMenuIcon, PersonIcon } from '@radix-icons/vue';
+import {
+    Half2Icon,
+    HamburgerMenuIcon,
+    MoonIcon,
+    PersonIcon,
+    SunIcon,
+} from '@radix-icons/vue';
+import { useColorMode } from '@vueuse/core';
 import { onMounted, onUnmounted, ref } from 'vue';
 
 const isMobileSidebarOpen = ref(false);
 const closeMobileSidebar = () => {
     isMobileSidebarOpen.value = false;
 };
+
+const colorMode = useColorMode({ emitAuto: true });
 
 onMounted(() => {
     window.addEventListener('resize', closeMobileSidebar);
@@ -30,19 +39,19 @@ onUnmounted(() => {
 
 <template>
     <div
-        class="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]"
+        class="grid h-screen w-full md:grid-cols-[224px_1fr] lg:grid-cols-[288px_1fr]"
     >
         <div class="hidden border-r bg-muted/40 md:block">
             <Sidebar />
         </div>
         <div class="flex flex-col">
             <header
-                class="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:px-6"
+                class="flex h-14 items-center gap-3 border-b bg-muted/40 px-4 lg:px-6"
             >
                 <Sheet v-model:open="isMobileSidebarOpen">
                     <SheetTrigger as-child>
                         <Button
-                            variant="outline"
+                            variant="ghost"
                             size="icon"
                             class="shrink-0 md:hidden"
                         >
@@ -57,9 +66,43 @@ onUnmounted(() => {
                 <DropdownMenu>
                     <DropdownMenuTrigger as-child>
                         <Button
-                            variant="secondary"
+                            variant="ghost"
                             size="icon"
                             class="ml-auto rounded-full"
+                        >
+                            <Half2Icon
+                                v-if="colorMode === 'auto'"
+                                class="h-5 w-5"
+                            />
+                            <SunIcon
+                                v-if="colorMode === 'light'"
+                                class="h-5 w-5"
+                            />
+                            <MoonIcon
+                                v-if="colorMode === 'dark'"
+                                class="h-5 w-5"
+                            />
+                            <span class="sr-only">Toggle theme</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem @click="colorMode = 'auto'">
+                            Auto
+                        </DropdownMenuItem>
+                        <DropdownMenuItem @click="colorMode = 'light'">
+                            Light
+                        </DropdownMenuItem>
+                        <DropdownMenuItem @click="colorMode = 'dark'">
+                            Dark
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <DropdownMenu>
+                    <DropdownMenuTrigger as-child>
+                        <Button
+                            variant="ghost"
+                            size="icon"
+                            class="rounded-full"
                         >
                             <PersonIcon class="h-5 w-5" />
                             <span class="sr-only">Toggle user menu</span>
