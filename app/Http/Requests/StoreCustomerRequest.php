758c47f6c432ebc40c29a72df48f3575ec09647d
Catalin Bratu <contact@catalinbratu.com>
2025-02-07 13:21:41 +0200

    feat: add validation rules for customer creation

--- app/Http/Requests/StoreCustomerRequest.php
+++ app/Http/Requests/StoreCustomerRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreCustomerRequest extends FormRequest
 {
@@ -14,7 +16,12 @@ class StoreCustomerRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'vat' => ['required', 'regex:/^[A-Z0-9]{8,20}$/', Rule::unique('customers')->where(fn (Builder $query) => $query->where('user_id', request()->user()->id))],
+            'email' => 'nullable|email|max:255',
+            'phone' => 'nullable|regex:/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/',
+            'iban' => 'nullable|regex:/^([A-Z]{2})(\d{2})([A-Z0-9]{4})([A-Z0-9]{4})([A-Z0-9]{4})([A-Z0-9]{4})([A-Z0-9]{0,4})$/',
+            'bank' => 'nullable|string|max:255',
         ];
     }
 }
